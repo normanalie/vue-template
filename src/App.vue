f367a1d2c1f6a5e6b429d5b2634c3404ebaca304
Norman ALIE <mail@normanalie.fr>
2023-12-06 21:03:57 +0100

    Init

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts">
+import {Button} from '@/components/ui/button'
+import { Calendar } from '@/components/ui/calendar'
+import {supabase} from '@/lib/supabase'
+import { onMounted } from 'vue';
+
+onMounted(()=>{
+  getData()
+})
+
+async function getData(){
+  const {data} = await supabase.from('products').select('*')
+  console.log(data)
+}
+</script>
+
+<template >
+  <div>
+    <Button>HEEEELELOOOO</Button>
+    <Calendar/>
+  </div>
+</template>
+
+<style scoped></style>
